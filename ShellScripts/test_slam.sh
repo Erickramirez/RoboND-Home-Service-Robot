d325d54e7acd51ab1bc786cec69b50e5a9e11221
Erick Ramirez <erickramireztebalan@gmail.com>
2018-08-10 20:46:25 +0000

    feat: adding parameters for gmapping

--- ShellScripts/test_slam.sh
+++ ShellScripts/test_slam.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 xterm  -e  " roslaunch turtlebot_gazebo turtlebot_world.launch world_file:=/home/workspace/catkin_ws/src/RoboND-Home-Service-Robot/World/room.world" &
 sleep 10
-xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch" & 
+xterm  -e  " roslaunch turtlebot_gazebo gmapping_demo.launch custom_gmapping_launch_file:=/home/nvidia/catkin_ws/src/RoboND-Home-Service-Robot/launch/gmapping.launch.xml" & 
 sleep 10
 xterm  -e  " roslaunch turtlebot_rviz_launchers view_navigation.launch" &
 sleep 10
